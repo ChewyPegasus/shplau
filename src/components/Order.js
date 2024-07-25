32a35b678df6e2b180e3076e3350f54b1dec28df
ChewyPegasus <neveroyatnyi.chelovek@gmail.com>
2024-07-25 17:50:29 +0300

    not working

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { MdOutlineDeleteForever } from "react-icons/md";
+import * as Quantifier from "./Quantifier/Quantifier.tsx"
 
 export class Order extends Component {
   render() {
@@ -8,6 +9,10 @@ export class Order extends Component {
         <img src={"./img/" + this.props.item.img} alt='' />
         <h2>{this.props.item.title}</h2>
         <b>{this.props.item.price}BYN</b>
+        <Quantifier
+              removeProductCallback={() => handleRemoveProduct(product.id)}
+              productId={product.id}
+              handleUpdateQuantity={handleUpdateQuantity} />
         <MdOutlineDeleteForever className='delete_icon' size={30} onClick={() => this.props.onDelete(this.props.item.id)}/>
       </div>
     )
